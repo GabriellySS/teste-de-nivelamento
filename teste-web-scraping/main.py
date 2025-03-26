4443f531dd64642edde1eefbb216ed361b732b88
Gabrielly Santos <gabriellysantos@Gabriellys-MacBook-Air.local>
2025-03-26 00:02:22 -0300

    Procurando pdf pelo texto da tag ancora

--- teste-web-scraping/main.py
+++ teste-web-scraping/main.py
@@ -4,6 +4,7 @@ Código desenvolvido para o download de determinados arquivos em pdf de um e com
 
 # Importando as bibliotecas
 import os
+import sys
 from urllib.parse import urljoin
 import requests
 from bs4 import BeautifulSoup
@@ -20,25 +21,43 @@ os.makedirs("downloads", exist_ok=True)
 # Listagem dos arquivos a serem baixados
 arquivosDesejados = {"Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf", "Anexo_II_DUT_2021_RN_465.2021_RN628.2025_RN629.2025.pdf"}
 
-# Encontrando os links dos arquivos pdf's
-for link in soup.find_all("a", href=True):
-    urlArquivo = link["href"]
-    if urlArquivo == "#" or urlArquivo.strip() == "":
-        continue
-    if not urlArquivo.startswith("http"):
-        urlArquivo = urljoin(urlPagina, urlArquivo)
-    nomeArquivo = urlArquivo.split("/")[-1]
-    if nomeArquivo in arquivosDesejados:
-        urlArquivo = urljoin(urlPagina, urlArquivo)
-        diretorioArquivo = os.path.join("downloads", nomeArquivo)
+# Função para realizar o download de arquivos.
+def download_arquivo(url_arquivo, caminho_arquivo):
+    respostaArquivo = requests.get(url_arquivo, stream=True)
+    if respostaArquivo.status_code == 200:
+        with open(caminho_arquivo, "wb") as file:
+            for chunk in respostaArquivo.iter_content(1024):
+                file.write(chunk)
+        print(f"Baixado: {caminho_arquivo}")
 
-# Fazendo o download dos arquivos
-respostaArquivo = requests.get(urlArquivo, stream=True)
-if respostaArquivo.status_code == 200:
-    with open(diretorioArquivo, "wb") as file:
-        for chunk in respostaArquivo.iter_content(1024):
-            file.write(chunk)
-    print(f"Baixado: {diretorioArquivo}")
+# Encontrando os links dos arquivos PDFs
+for link in soup.find_all("a", href=True):
+    # print(dir(link))
+    # print(link.text)
+
+    if link.text == "Anexo I.":
+        print("Achei o primeiro PDF. Anexo I.")
+        url_arquivo = link["href"]
+        nome_arquivo = url_arquivo.split("/")[-1]
+
+        print(f"URL arquivo: {url_arquivo}")
+        print(f"Nome arquivo: {nome_arquivo}")
+
+        caminho_arquivo = os.path.join("downloads", nome_arquivo)
+        download_arquivo(url_arquivo, caminho_arquivo)
+
+    if link.text == "Anexo II.":
+        print("Achei o segundo PDF. Anexo II.")
+        url_arquivo = link["href"]
+        nome_arquivo = url_arquivo.split("/")[-1]
+
+        print(f"URL arquivo: {url_arquivo}")
+        print(f"Nome arquivo: {nome_arquivo}")
+
+        caminho_arquivo = os.path.join("downloads", nome_arquivo)
+        download_arquivo(url_arquivo, caminho_arquivo)
+
+        break
 
 # Compactando a pasta downloads
 shutil.make_archive("downloads", "zip", "downloads")
